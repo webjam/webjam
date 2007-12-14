26a1f425d9e3c72c700f84067bded5d0d7c6073b
Tim Lucas <t.lucas@toolmantim.com>
2007-12-14 20:39:25 +1100

    Made the openid_auth_mock protected

--- lib/openid_auth_mock.rb
+++ lib/openid_auth_mock.rb
@@ -1,5 +1,6 @@
 OpenIdAuthentication.module_eval do
-  def authenticate_with_open_id(identity_url, fields)
-    yield OpenIdAuthentication::Result[:successful], identity_url, nil
-  end
+  protected
+    def authenticate_with_open_id(identity_url, fields)
+      yield OpenIdAuthentication::Result[:successful], identity_url, nil
+    end
 end
