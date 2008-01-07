fb69b055dfaa97bc56282964ca282899cd45ab90
Tim Lucas <t.lucas@toolmantim.com>
2008-01-07 18:11:21 +1100

    Get profile updating working, and whilst we're there move to using /account instead of /user/edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,19 @@ ActionController::Routing::Routes.draw do |map|
     posts.resources :comments, :name_prefix => 'post_'
   end
   map.resources :comments
-  map.resources :events, :collection => [:past]
+#  map.resources :events, :collection => [:past]
   map.resource  :session, :member => { :create => :any }
-  map.resource  :user, :name_prefix => 'current_', :member => {:update_privacy => :put, :update_profile_details => :put}
+  
+  map.with_options(:controller => "users") do |user|
+    user.update_profile_details_current_user 'account/update_profile_details', :conditions => {:method => :put}, :action => "update_profile_details"
+    user.update_privacy_current_user 'account/update_privacy', :conditions => {:method => :put}, :action => "update_profile_details"
+    user.edit_current_user 'account', :conditions => {:method => :get}, :action => "edit"
+  end  
   map.resources :users, :collection => {:verify => :any, :details => :get, :create => :post}
+  
   map.resource  :mugshot, :name_prefix => 'current_'
   map.resources :mugshots, :member => {:crop => :any}
+
   map.resources :identity_urls, :collection => {:create => :any}
   map.with_options(:controller => 'pages') do |m|
     m.about   'about',   :action => 'about'
