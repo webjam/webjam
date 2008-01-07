9f109753351150376409ac469e3390a98bf9acb8
Tim Lucas <t.lucas@toolmantim.com>
2008-01-07 15:39:13 +1100

    Update deploy script, set ActionMailer to use sendmail and add a maintenance page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,5 +14,6 @@ config.action_controller.perform_caching             = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = false
+config.action_controller.session_store = :mem_cache_store
+config.action_mailer.delivery_method = :sendmail
