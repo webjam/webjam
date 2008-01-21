89afb0536af7038eba342586551aab71c69efd89
Lachlan Hardy <lachlan@lachstock.com.au>
2008-01-21 13:33:48 +1100

    Adding flat file for building archives

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :identity_urls, :collection => {:create => :any}
   map.with_options(:controller => 'pages') do |m|
     m.about   'about',   :action => 'about'
+    m.archiveflat   'archive-flat',   :action => 'archive-flat' #temporary flat files to build out front-end code
     m.contact 'contact', :action => 'contact'
     m.home    '',        :action => 'home'
     m.about   'openid',   :action => 'openid'
