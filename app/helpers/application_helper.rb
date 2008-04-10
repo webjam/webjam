3848fe53cbbbd3ac899a20214f64f29d49faa13f
Lachlan Hardy <lachlan@lachstock.com.au>
2008-04-11 08:34:56 +1000

    styling and helper for user login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,30 +1,26 @@
 module ApplicationHelper
- # def user_link_with_avatar(user, classes="")
- #   classes += " user-avatar"
- #   link_to "<span class='vcard'>#{avatar_image(user)}&nbsp;<span class='fn nickname'>#{h(user.nick_name)}</span></span>", user_path(user), :class => classes.strip
- # end
 
   def user_link_with_avatar(user, classes="")
     render :partial => "shared/user_avatar",
            :locals => {:user => user, :classes => classes}
   end
-  
-  
+
   def user_login_link_with_avatar(user)
-    link_to "Hi #{avatar_image(user)}&nbsp;<strong class='fn nickname'>#{h(user.nick_name)}</strong>!", user_path(user), :class => 'user-avatar vcard'
+    # link_to "Hi #{avatar_image(user)}&nbsp;<strong class='fn nickname'>#{h(user.nick_name)}</strong>!", user_path(user), :class => 'user-avatar vcard'
+    render :partial => "shared/user_login_avatar",
+           :locals => {:user => user}
   end
+  
   def avatar_image(user, size=:tiny)
     raise(ArgumentError, "user can not be nil") if user.nil?
     image_tag(user.mugshot ? user.mugshot.public_filename(size) : "default_avatar_#{size || 'large'}.gif", :alt => h(user.nick_name), :class => 'photo')
   end
+  
+  
   # I just know there's a better way to do this... Help?
   def user_link_for_comment(user, classes="")
     classes += " user-avatar vcard"
-    link_to "#{comment_avatar(user)}&nbsp;<span class='fn nickname'>#{h(user.nick_name)}</span>", user_path(user), :class => classes.strip
-  end
-  def comment_avatar(user, size=:medium)
-    raise(ArgumentError, "user can not be nil") if user.nil?
-    image_tag(user.mugshot ? user.mugshot.public_filename(size) : "default_avatar_#{size || 'large'}.gif", :alt => h(user.nick_name), :class => 'photo')
+    link_to "#{avatar_image(user, :medium)}&nbsp;<span class='fn nickname'>#{h(user.nick_name)}</span>", user_path(user), :class => classes.strip
   end
   # end lameness ;)
   
