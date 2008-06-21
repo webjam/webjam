057464fb9af7f8b785b40c15ffba83578198fe5d
Tim Lucas <t.lucas@toolmantim.com>
2008-06-22 04:39:42 +1000

    Add Event#presentation_proposals

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base
   has_many :rsvps
+  has_many :presentation_proposals
   has_many :users, :through => :rsvps
+
   def upcoming?(now=Time.now.utc)
     self.held_at >= now
   end
