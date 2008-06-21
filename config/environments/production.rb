cbf1592d8401dc1fd88c3a5f817bbabd198f66a2
Tim Lucas <t.lucas@toolmantim.com>
2008-06-21 21:09:10 +1000

    Move to Rails 2.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,10 +10,13 @@ config.cache_classes = true
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
+config.action_view.cache_template_loading            = true
+
+# Use a different cache store in production
+# config.cache_store = :mem_cache_store
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
-config.action_mailer.raise_delivery_errors = false
-config.action_controller.session_store = :mem_cache_store
-config.action_mailer.delivery_method = :sendmail
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
