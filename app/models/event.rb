72cfa24b5b0eae2397f8c56537d8705736929157
Dylan Egan <degan@engineyard.com>
2008-06-22 03:58:10 +1000

    Add rsvps => users association for event. full? method for checking rsvp limit.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,12 @@
 class Event < ActiveRecord::Base
+  has_many :rsvps
+  has_many :users, :through => :rsvps
   def upcoming?(now=Time.now.utc)
     # TODO: Impelement
     true
   end
+
+  def full?
+    rsvps.size >= rsvp_limit
+  end 
 end
