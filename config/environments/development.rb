4735ee9e9dc849d970bef497913ea98ddefbe220
Tim Lucas <t.lucas@toolmantim.com>
2008-06-21 23:14:41 +1000

    Mock out OpenID for development purposes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,12 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+config.after_initialize do
+  OpenIdAuthentication.class_eval do
+    protected
+      def authenticate_with_open_id(identity_url = params[:openid_url], options = {}, &block) #:doc:
+        yield OpenIdAuthentication::Result[:successful], identity_url, nil
+      end
+  end
+end
