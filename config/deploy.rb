7bb1dde5873d2fd60bc95f31e7c76d954e29b13e
Lincoln Stoll <lstoll@lstoll.net>
2008-06-24 17:37:57 +1000

    * correcting typos * adding task to install required gems on remote server * adding method to link to the remote database config into the current application * setting rake to the ruby enterprise installed rake

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,10 @@
-set :stages, %w(staging production egde)
+set :stages, %w(staging production edge)
 set :default_stage, "edge"
 require 'capistrano/ext/multistage'
 
 # Common stuff goes here, like perms, servers, and restart stuff
+
+set :ssh_options, { :forward_agent => true } # this is so we don't need a appdeploy key
 
 set :application, "webjam"
 
@@ -13,16 +15,28 @@ role :app, "208.75.86.29"
 role :web, "208.75.86.29"
 role :db, "208.75.86.29", :primary => true
 
+set :rake, "/opt/ruby-enterprise-1.8.6-20080507/bin/rake"
 
 set :app_user, "webjamapp"
 
-set :group, "appdeploy"
+set :group, "webjamdeploy"
 
+# TODO - investigate security implications. I doubt there is any.
 def set_remote_permissions
   sudo <<-CMD
-  sh -c "chown -R #{app_user}:#{group} #{release_path} &&
-  chmod -R g+w #{release_path}"
+  sh -c "chown -R #{app_user}:#{group} #{deploy_to} &&
+  chmod -R g+w #{deploy_to}"
   CMD
+end
+
+def link_database_config
+  sudo "ln -s #{deploy_to}/../config/database.yml #{release_path}/config/database.yml"
+end
+
+def install_remote_gems
+  rake = fetch(:rake, "rake")
+  rails_env = fetch(:rails_env, "production")
+  sudo "sh -c \"cd #{current_release}; #{rake} RAILS_ENV=#{rails_env} gems:install\""
 end
 
 namespace :deploy do
--- config/deploy/edge.rb
+++ config/deploy/edge.rb
@@ -1,15 +1,14 @@
-# STAGING-specific deployment configuration
+# EGDE-specific deployment configuration
 # please put general deployment config in config/deploy.rb
 
-set :deploy_to, "/srv/webjam/staging"
+set :deploy_to, "/srv/webjam/edge"
 
-set :branch, "production"
+set :branch, "master"
 
-set :rails_env, "staging"
+set :rails_env, "edge"
 
 task :after_update_code, :roles => :app do
   link_database_config
   install_remote_gems
   set_remote_permissions
-  # remote all tables in this database
 end
