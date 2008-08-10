a5c05694a5cf2900b543c0d4e2a1b94ac10d71ff
Dylan Egan <degan@engineyard.com>
2008-08-10 12:34:42 +1000

    Remove dat shit.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,6 @@ class PagesController < ApplicationController
     @upcoming_events = Event.published.upcoming(:order => "held_at DESC")
     @past_events = Event.published.past(:order => "held_at DESC")
     @latest_post = Post.published.latest 
-    @latest_posts = Post.published.latest(5)
     if iphone_request?
       render :action => "home_iphone", :layout => "iphone"
     end
