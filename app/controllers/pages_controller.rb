a150ed0b5ae345ba525da2f2e43ed6eb493e3c30
Dylan Egan <degan@engineyard.com>
2008-08-10 12:25:16 +1000

    Use new method.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     @upcoming_events = Event.published.upcoming(:order => "held_at DESC")
     @past_events = Event.published.past(:order => "held_at DESC")
-    @latest_post = Post.published.first(:order => "published_at DESC", :include => :comments) 
+    @latest_post = Post.published.latest 
     if iphone_request?
       render :action => "home_iphone", :layout => "iphone"
     end
