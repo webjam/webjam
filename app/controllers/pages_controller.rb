f8afc7d5f59ab5d0ea524a8d62ac43f12a6178af
Dylan Egan <degan@engineyard.com>
2008-08-10 12:18:52 +1000

    Get latest post.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,9 @@ class PagesController < ApplicationController
   def home
     @upcoming_events = Event.published.upcoming(:order => "held_at DESC")
     @past_events = Event.published.past(:order => "held_at DESC")
+    @latest_post = Post.published.first(:order => "published_at DESC", :include => :comments) 
     if iphone_request?
       render :action => "home_iphone", :layout => "iphone"
-    else
-      render
     end
   end
   def statichome
