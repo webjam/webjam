0aba214dad73bf845500e9a25e9cd3d7b1759923
Tim Lucas <t.lucas@toolmantim.com>
2008-09-06 13:56:53 +1000

    Don't raise on RSVP validation error

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -7,7 +7,7 @@ class RsvpsController < ApplicationController
     if @event.full?
       redirect_to event_path(:id => @event, :event_full => true)
     end
-    if current_user.rsvps.create!(:event => @event)
+    if current_user.rsvps.create(:event => @event)
       redirect_to event_path(@event)
     else
       redirect_to event_path(:id => @event, :already_rsvpd => true)
