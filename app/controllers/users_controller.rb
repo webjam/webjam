bfd8b90a700b7a74ff4b01ad7ef78987d9e9e0d8
Tim Lucas <t.lucas@toolmantim.com>
2008-09-10 00:43:46 +1000

    Added a /people page with a paginated list of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
   
   def index
-    redirect_to home_path, :status => 302
+    @users = User.paginate(:page => params[:page], :order => "nick_name ASC", :per_page => 50)
   end
   
   def new
