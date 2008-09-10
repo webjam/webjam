8a5de8cb6ac9645006853076a24a50cf024178c1
Lincoln Stoll <lstoll@lstoll.net>
2008-09-10 15:39:05 +1000

    Adding the presented feed URL to the application config - if not set, defaults to '/news.atom'

--- lib/application_config.rb
+++ lib/application_config.rb
@@ -4,7 +4,8 @@ class ApplicationConfig
   attr_reader :flickr_api_key,
               :jeeves_campfire_password,
               :campaign_monitor_api_key,
-              :session_secret
+              :session_secret,
+              :feed_url
   
   def initialize
     y = YAML::load(File.open(File.dirname(__FILE__) + '/../config/application.yml'))
@@ -12,5 +13,6 @@ class ApplicationConfig
     @jeeves_campfire_password = y['jeeves_campfire_password']
     @campaign_monitor_api_key = y['campaign_monitor_api_key']
     @session_secret = y['session_secret']
+    @feed_url = y['feed_url'] ? y['feed_url'] : "/news.atom"
   end
 end
