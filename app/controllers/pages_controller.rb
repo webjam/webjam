918c2be2e1653a8aeea1c0139b469c5fb1ef7132
Tim Lucas <t.lucas@toolmantim.com>
2008-09-10 16:00:46 +1000

    Remove iphone page for iphone users until we actually put something on the page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,9 @@ class PagesController < ApplicationController
   def home
     @upcoming_events = Event.published.upcoming(:order => "held_at DESC")
     @past_events = Event.published.past(:order => "held_at DESC")
-    if iphone_request?
-      render :action => "home_iphone", :layout => "iphone"
-    end
+    # if iphone_request?
+    #   render :action => "home_iphone", :layout => "iphone"
+    # end
   end
   def contact
   end
