6677d113971ce308c0e578f054b6cf93e1e4db0e
Lachlan Hardy <lachlan@lachstock.com.au>
2008-09-16 07:23:40 +1000

    hotted up People page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,12 @@ module ApplicationHelper
            :locals => {:user => user, :classes => classes}
   end
 
-  def user_login_link_with_avatar(user)
-    render :partial => "shared/user_login_avatar",
+  def peeps_user_link_with_avatar(user)
+    render :partial => "shared/peeps_user_avatar",
            :locals => {:user => user}
   end
   
-  def avatar_image(user, size=:tiny)
+  def avatar_image(user, size)
     raise(ArgumentError, "user can not be nil") if user.nil?
     # image_tag(user.mugshot ? user.mugshot.url(size) : "default_avatar_#{size || 'large'}.gif", :alt => h(user.nick_name), :class => 'photo')
     image_tag(user.mugshot.url(size), :alt => h(user.nick_name), :class => 'photo')
