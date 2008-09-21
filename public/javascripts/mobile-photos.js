3e4d7229d6d3b2d6976746e798ad904aac68aaa6
Tim Lucas <t.lucas@toolmantim.com>
2008-09-21 22:50:37 +1000

    Handle race conditions with when loading multiple images.

--- public/javascripts/mobile-photos.js
+++ public/javascripts/mobile-photos.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
   $("ul.photo-list li a").click(function() {
-    var $currentZoomedImage = $(".zoomedImage");
-    if ($currentZoomedImage.length != 0) {
-      $currentZoomedImage.remove();
+    var currentZoomedImage = $(".zoomedImage");
+    if (currentZoomedImage) {
+      currentZoomedImage.remove();
     }
     
     var $thumbnailAnchor = $(this);
     $thumbnailAnchor.append($("<div class='spinner'></div>"));
 
-    var imageSrc = $(this).children("img").attr("src").replace("_s.jpg","_m.jpg");
+    var imageSrc = $thumbnailAnchor.children("img").attr("src").replace("_s.jpg","_m.jpg");
     var $image = $("<img/>").attr("src", imageSrc);
 
-    var $imageContainer = $("<div class='zoomedImage' style='display:none'></div>");
+    var $imageContainer = $("<div class='zoomedImage'></div>");
     var $closeLayer = $("<div class='close'/>");
     var $imageInner = $("<div class='inner'></div>");
-    var $externalLink = $("<div class='externalLink'><a href='#' class='button'>View on flickr</a>");
-    $imageInner.append($image).append($closeLayer);
-    $imageContainer.append($imageInner).append($externalLink);
+    var $externalLink = $("<div class='externalLink'><a href='#' class='button'><span class='text'>" + $thumbnailAnchor.find(".title").text() + $thumbnailAnchor.find(".by").text() + "</span>"  + "</a>");
+    
+    $imageInner.append($image).append($closeLayer).append($externalLink);
+    $imageContainer.append($imageInner);
 
     var close = function() {
-      $(document.body).data('zoomedImage', null);
       $imageContainer.remove();
       return false;
     }
@@ -27,16 +27,17 @@ $(document).ready(function() {
     $closeLayer.click(close);
     $image.click(close);
     
-    $image.load(function() {
-      console.log("window height: " + $(window).height());
-      console.log("image height: " + $image.attr("height"));
-      console.log("window.pageYOffset: " + window.pageYOffset);
-      $imageContainer.css('top', ($(window).height() / 2) - ($image.attr("height") / 2) + window.pageYOffset - 25);
-      $thumbnailAnchor.children(".spinner").remove();
-      $imageContainer.show();
-    });
+    $(document).data('latestImageContainer', $imageContainer);
     
-    $(document.body).append($imageContainer);
+    $image.load(function() {
+      $thumbnailAnchor.children(".spinner").remove();
+      // Check for race condition with another image loading
+      if ($(document).data('latestImageContainer') == $imageContainer) {
+        $imageInner.css({height:$image.attr("height"), width:$image.attr("width")});
+        $imageContainer.css('top', ($(window).height() / 2) - ($image.attr("height") / 2) + window.pageYOffset - 25);
+        $(document.body).append($imageContainer);
+      }
+    });
 
     return false;
   });
