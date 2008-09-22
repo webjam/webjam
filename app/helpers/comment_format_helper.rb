236061a575ee3d65dcd6dc22b7f95bf3b2894422
Tim Lucas <t.lucas@toolmantim.com>
2008-09-23 01:42:14 +1000

    Style the blog post comments all nicely

--- app/helpers/comment_format_helper.rb
+++ app/helpers/comment_format_helper.rb
@@ -1,5 +1,5 @@
 module CommentFormatHelper
   def format_comment_body(body)
-    sanitize simple_format(body), :tags => %w(strong em a code pre blockquote span q abbr acronym br img ul ol li del), :attributes => %w(href class cite rel rev title src alt)
+    sanitize simple_format(body), :tags => %w(p strong em a code pre blockquote span q abbr acronym br img ul ol li del br), :attributes => %w(href class cite rel rev title src alt)
   end
 end
