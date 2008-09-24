61d7f3108f14922b749768cc8695e5241f149d35
Tim Lucas <t.lucas@toolmantim.com>
2008-09-24 18:19:31 +1000

    Move static stuff into partials (oh so hacky)

---
+++ app/helpers/event_partial_content_helper.rb
@@ -0,0 +1,17 @@
+module EventPartialContentHelper
+  def sponsor_content(event)
+    if File.exist?("#{RAILS_ROOT}/app/views/events/#{event.tag}/_sponsors.html.haml")
+      render :partial => "events/#{event.tag}/sponsors.html.haml"
+    end
+  end
+  def winners_content(event)
+    if File.exist?("#{RAILS_ROOT}/app/views/events/#{event.tag}/_winners.html.haml")
+      render :partial => "events/#{event.tag}/winners.html.haml"
+    end
+  end
+  def event_summary(event)
+    if File.exist?("#{RAILS_ROOT}/app/views/events/#{event.tag}/_summary.html.haml")
+      render :partial => "events/#{event.tag}/summary.html.haml"
+    end
+  end
+end
