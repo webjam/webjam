9cd4c1d7dfdefc4360912d41267edd6c81f46963
Tim Lucas <t.lucas@toolmantim.com>
2008-09-28 16:26:11 +1000

    Change the story helper to make sure NotFound handling isn't bypassed in stories

--- stories/helper.rb
+++ stories/helper.rb
@@ -6,6 +6,21 @@ Dir[File.join(File.dirname(__FILE__), "steps/*.rb")].each do |file|
   require file
 end
 
+# Added these so that exceptions that occur in stories aren't handled by rails
+ActionController::Base.class_eval do
+  def perform_action_with_rescue #:nodoc:
+    perform_action_without_rescue
+  rescue Exception => exception
+    rescue_action_with_handler(exception) || raise
+  end
+end
+
+Dispatcher.class_eval do
+  def self.failsafe_response(output, status, exception = nil)
+    raise exception
+  end
+end
+
 require File.join(File.dirname(__FILE__), "webjam_story_fixtures")
 
 Spec::Story::World.class_eval do
