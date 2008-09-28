37a8fcc5549214d8f5a5fd818bd3750111ccb2b4
Tim Lucas <t.lucas@toolmantim.com>
2008-09-28 22:09:39 +1000

    Added stories for account editing and updating

---
+++ features/steps/account_updating_steps.rb
@@ -0,0 +1,25 @@
+When "I view the account page" do
+  get edit_current_user_path
+end
+
+When "I update my account details" do
+  @new_account_details = {
+    :nick_name => "new_nick_name",
+    :full_name => "New full name",
+    :email => "new_email@address.com",
+    :website_name => "New website name",
+    :website_url => "http://new.website.com/",
+  }
+  put update_profile_details_current_user_path(:user => @new_account_details)
+end
+
+Then "my account details are updated" do
+  @logged_in_user.reload
+  [:nick_name, :full_name, :email, :website_name, :website_url].each do |attr|
+    @logged_in_user[attr].should == @new_account_details[attr]
+  end
+end
+
+Then "I am redirected to the account page" do
+  response.should redirect_to(edit_current_user_path)
+end
--- features/steps/posts_steps.rb
+++ features/steps/posts_steps.rb
@@ -1,9 +1,3 @@
-Given "I am logged in" do
-  user = Factory.create(:user)
-  Factory.create(:identity_url, :user => user)
-  login(user)
-end
-
 When "I view the mobile post page" do
   post = Factory.create(:post)
   get formatted_post_path(post.year, post.permalink, :mobile)
