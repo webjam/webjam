2503a2ef27cea505d67bd65df27201c21b2f2165
Tim Lucas <t.lucas@toolmantim.com>
2008-09-28 23:50:58 +1000

    Show the photos for the event, not just previous

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,27 +5,21 @@ class EventsController < ApplicationController
     @total_tweets = @event.tweets.count
     @latest_tweets = @event.tweets.latest(5).all
     @more_tweets = @event.tweets.count > @latest_tweets.length
-    respond_to do |wants|
-      wants.html do
-        render :action => (@event.upcoming? ? "show_upcoming" : "show")
+    @all_photos = @event.flickr_photos.latest.all(:order => "created_at DESC")
+    if @event.upcoming?
+      @previous_event = @event.previous
+      @photos = @event.flickr_photos.latest.all :order => "created_at DESC", :limit => 10
+      @latest_photo = @photos.first
+      if @photos.empty? && @previous_event
+        @photos_from_previous_event = true
+        @photos = @previous_event.flickr_photos.featured.all :order => "created_at DESC", :limit => 10
+        @latest_photo = @photos.first
       end
-      wants.mobile do
-        if @event.upcoming?
-          @previous_event = @event.previous
-          @photos = @event.flickr_photos.latest.all :order => "created_at DESC", :limit => 10
-          @latest_photo = @photos.first
-          if @photos.empty? && @previous_event
-            @photos_from_previous_event = true
-            @photos = @previous_event.flickr_photos.featured.paginate :page => 1, :order => "created_at DESC"
-            @latest_photo = @photos.first
-          end
-          render :action => "show_upcoming"
-        else
-          @photos = @event.flickr_photos.latest.all :order => "created_at DESC", :limit => 10
-          @latest_photo = @photos.first
-          render :action => "show"
-        end
-      end
+      render :action => "show_upcoming"
+    else
+      @photos = @event.flickr_photos.latest.all :order => "created_at DESC", :limit => 10
+      @latest_photo = @photos.first
+      render :action => "show"
     end
   end
   def past
