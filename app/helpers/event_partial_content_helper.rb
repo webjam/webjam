68d4b49516447b1c32dfdff298633b39528f5999
Lachlan Hardy <lachlan@lachstock.com.au>
2008-09-29 15:18:59 +1000

    updating sponsorship mojo

--- app/helpers/event_partial_content_helper.rb
+++ app/helpers/event_partial_content_helper.rb
@@ -4,6 +4,11 @@ module EventPartialContentHelper
       render :partial => "events/#{event.tag}/sponsors.html.haml"
     end
   end
+  def sponsor_major_content(event)
+    if File.exist?("#{RAILS_ROOT}/app/views/events/#{event.tag}/_sponsors_major.html.haml")
+      render :partial => "events/#{event.tag}/sponsors_major.html.haml"
+    end
+  end
   def winners_content(event)
     if File.exist?("#{RAILS_ROOT}/app/views/events/#{event.tag}/_winners.html.haml")
       render :partial => "events/#{event.tag}/winners.html.haml"
