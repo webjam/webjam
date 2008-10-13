0224a39a851f5e19f619a88976cc0810b01fb67b
Lachlan Hardy <lachlan@lachstock.com.au>
2008-10-14 08:12:05 +1100

    updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081004041035) do
+ActiveRecord::Schema.define(:version => 20081012151751) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id",          :limit => 11
@@ -78,6 +78,9 @@ ActiveRecord::Schema.define(:version => 20081004041035) do
     t.datetime "updated_at"
     t.integer  "number_of_presenters",     :limit => 11
     t.integer  "number",                   :limit => 11
+    t.datetime "published_at"
+    t.integer  "place",                    :limit => 11
+    t.string   "prize"
   end
 
   create_table "jams_users", :id => false, :force => true do |t|
